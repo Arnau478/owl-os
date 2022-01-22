54e673c070392d8b3604fd9744b3bcf1e0ab5554
Arnau478 <arnauxabia@gmail.com>
2022-01-22 02:08:08 +0100

    Defined and implemented align() utility

---
+++ src/bootloader/stage2/util.c
@@ -0,0 +1,8 @@
+#include "util.h"
+
+uint32_t align(uint32_t number, uint32_t alignTo){
+    if(alignTo == 0) return number;
+
+    uint32_t rem = number % alignTo;
+    return (rem > 0) ? (number + alignTo - rem) : number;
+}
