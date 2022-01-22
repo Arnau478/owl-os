53da091cb9cbb0aea1d45c420d36908041010f47
Arnau478 <arnauxabia@gmail.com>
2022-01-22 02:41:02 +0100

    Disk reads now do not care about output pointer type

--- src/bootloader/stage2/disk.c
+++ src/bootloader/stage2/disk.c
@@ -30,7 +30,7 @@ void DISK_LBA2CHS(DISK* disk, uint32_t lba, uint16_t* cylinderOut, uint16_t* sec
     *headOut = (lba / disk->sectors) % disk->heads;
 }
 
-bool DISK_ReadSectors(DISK *disk, uint32_t lba, uint8_t sectors, uint8_t far *dataOut){
+bool DISK_ReadSectors(DISK *disk, uint32_t lba, uint8_t sectors, void far *dataOut){
     uint16_t cylinder, sector, head;
     
     DISK_LBA2CHS(disk, lba, &cylinder, &sector, &head);
