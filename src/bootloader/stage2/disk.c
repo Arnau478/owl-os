444ef4ab1adec8c9b61d83f15ad71715b04b5193
Arnau478 <arnauxabia@gmail.com>
2022-01-22 20:25:10 +0100

    Fixed DISK_Initialize typo

--- src/bootloader/stage2/disk.c
+++ src/bootloader/stage2/disk.c
@@ -5,16 +5,14 @@ bool DISK_Initialize(DISK *disk, uint8_t driveNumber){
     uint8_t driveType;
     uint16_t cylinders, sectors, heads;
 
-    disk->id = driveNumber;
-
     if(!x86_Disk_GetDriveParams(disk->id, &driveType, &cylinders, &sectors, &heads)){
         return false;
     }
 
     disk->id = driveNumber;
-    disk->cylinders = cylinders;
+    disk->cylinders = cylinders + 1;
+    disk->heads = heads + 1;
     disk->sectors = sectors;
-    disk->heads = heads;
     
     return true;
 }
