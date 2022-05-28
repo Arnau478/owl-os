c2efab6c20f9445b64f76f30ea84e47f84de5ece
Arnau478 <arnauxabia@gmail.com>
2022-05-29 01:28:36 +0200

    Fixed bug

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -8,7 +8,7 @@ extern uint8_t __end;
 
 void __attribute__((section(".entry"))) start(uint16_t bootDrive)
 {
-    memset(__bss_start, 0, (__end) - (__bss_start));
+    memset(&__bss_start, 0, (__end) - (__bss_start));
 
     HAL_Initialize();
 
