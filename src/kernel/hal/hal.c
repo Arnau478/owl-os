5c6f83eed1eaba1cd2bf27172b88d97cd483feba
Arnau478 <arnauxabia@gmail.com>
2022-06-04 03:11:57 +0200

    Initialized keyboard on HAL

--- src/kernel/hal/hal.c
+++ src/kernel/hal/hal.c
@@ -3,10 +3,12 @@
 #include <arch/i686/idt.h>
 #include <arch/i686/isr.h>
 #include <arch/i686/timer.h>
+#include <arch/i686/keyboard.h>
 
 void HAL_Initialize(){
     i686_GDT_Initialize();
     i686_IDT_Initialize();
     i686_ISR_Initialize();
     i686_Timer_Initialize(50);
+    i686_Keyboard_Initialize();
 }
